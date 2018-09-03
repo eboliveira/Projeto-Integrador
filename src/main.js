f8cd1edef00b24fe76b776eb8dfa98eae699dd12
eduh1997 <eduardooliveira.1997@alunos.utfpr.edu.br>
2018-09-03 19:03:34 -0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+
+// LightBootstrap plugin
+import LightBootstrap from './light-bootstrap-main'
+
+// router setup
+import routes from './routes/routes'
+// plugin setup
+Vue.use(VueRouter)
+Vue.use(LightBootstrap)
+
+// configure router
+const router = new VueRouter({
+  routes, // short for routes: routes
+  linkActiveClass: 'nav-item active'
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
