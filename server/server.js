9bd9ba60f0a09c5c9da9b99f83e25b164e1c191c
eduh1997 <eduardooliveira.1997@alunos.utfpr.edu.br>
2018-09-12 12:28:26 -0300

    model database accepts formated data from csv, routes doesn't completed yet

--- server/server.js
+++ server/server.js
@@ -10,20 +10,23 @@ app.use(bodyParser.json())
 app.use(cors())
 
 loadRoutes(app,'/')
+port = 8081
 
 mongoose.connect('mongodb://localhost:27017/JEED');
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function(callback){
-    console.log("Connection Succeeded");
+    console.log("Connection with mongo succeeded");
 });
+console.log("Server running on port: " + port)
+app.listen(process.env.PORT || port)
 
 app.get('/', function (req, res) {
     res.send('Root')
+})
+app.get('/generateDB', function (req, res) {
 })
 
 app.get('*', function (req, res) {
     res.redirect('/')
 })
-
-app.listen(process.env.PORT || 8081)
