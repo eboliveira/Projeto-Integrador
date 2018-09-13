6647174096b70de63e195129bd68249e887ae66d
Elivelton Fernando <eliveltonfoliveira@gmail.com>
2018-09-13 12:27:43 -0300

    Creation of users list

--- application/src/components/Dashboard/Views/UserProfile.vue
+++ application/src/components/Dashboard/Views/UserProfile.vue
@@ -2,30 +2,55 @@
   <div class="content">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-md-8">
-          <edit-profile-form>
-          </edit-profile-form>
-        </div>
-        <div class="col-md-4">
-          <user-card>
-          </user-card>
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h4 class="card-title">Usuários</h4>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-hover table-striped">
+                  <thead>
+                    <td>Nome</td>
+                    <td>Departamento</td>
+                    <td>Cargo</td>
+                    <td></td>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>José da Silva Sauro</td>
+                      <td>DACOM</td>
+                      <td>Professor</td>
+                      <td>
+                        <button class="btn btn-warning nc-settings-90 nc-icon"></button>
+                        <button class="btn btn-danger nc-scissors nc-icon"></button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Maria Juana da Silva</td>
+                      <td>DALIM</td>
+                      <td>Professora</td>
+                      <td>
+                        <button class="btn btn-warning nc-settings-90 nc-icon"></button>
+                        <button class="btn btn-danger nc-scissors nc-icon"></button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>João José</td>
+                      <td>DIRPLAD</td>
+                      <td>SERVIDOR</td>
+                      <td>
+                        <button class="btn btn-warning nc-settings-90 nc-icon"></button>
+                        <button class="btn btn-danger nc-scissors nc-icon"></button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
-<script>
-  import EditProfileForm from './UserProfile/EditProfileForm.vue'
-  import UserCard from './UserProfile/UserCard.vue'
-
-  export default {
-    components: {
-      EditProfileForm,
-      UserCard
-    }
-  }
-
-</script>
-<style>
-
-</style>
