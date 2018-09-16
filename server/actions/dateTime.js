bc7b56eef0ba9c32ab3499a5fbda7256172db08f
dsambugaro <daniloc@alunos.utfpr.edu.br>
2018-09-16 18:33:53 -0300

    detect conflict with events and adjusting schedules

--- server/actions/dateTime.js
+++ server/actions/dateTime.js
@@ -5,72 +5,72 @@ const moment = require('moment');
 
 const classSchedules = {
   M1: {
-    start: '07:30',
-    end: '08:20'
+    start: '07:30:00.000Z',
+    end: '08:20:00.000Z'
   },
   M2: {
-    start: '08:20',
-    end: '09:10'
+    start: '08:20:00.000Z',
+    end: '09:10:00.000Z'
   },
   M3: {
-    start: '09:10',
-    end: '10:00'
+    start: '09:10:00.000Z',
+    end: '10:00:00.000Z'
   },
   M4: {
-    start: '10:20',
-    end: '11:10'
+    start: '10:20:00.000Z',
+    end: '11:10:00.000Z'
   },
   M5: {
-    start: '11:10',
-    end: '12:00'
+    start: '11:10:00.000Z',
+    end: '12:00:00.000Z'
   },
   M6: {
-    start: '12:00',
-    end: '12:50'
+    start: '12:00:00.000Z',
+    end: '12:50:00.000Z'
   },
   T1: {
-    start: '13:00',
-    end: '13:50'
+    start: '13:00:00.000Z',
+    end: '13:50:00.000Z'
   },
   T2: {
-    start: '13:50',
-    end: '14:40'
+    start: '13:50:00.000Z',
+    end: '14:40:00.000Z'
   },
   T3: {
-    start: '14:40',
-    end: '15:30'
+    start: '14:40:00.000Z',
+    end: '15:30:00.000Z'
   },
   T4: {
-    start: '15:50',
-    end: '16:40'
+    start: '15:50:00.000Z',
+    end: '16:40:00.000Z'
   },
   T5: {
-    start: '16:40',
-    end: '17:30'
+    start: '16:40:00.000Z',
+    end: '17:30:00.000Z'
   },
   T6: {
-    start: '17:30',
-    end: '18:20'
+    start: '17:30:00.000Z',
+    end: '18:20:00.000Z'
   },
   N1: {
-    start: '18:40',
-    end: '19:30'
+    start: '18:40:00.000Z',
+    end: '19:30:00.000Z'
   },
   N2: {
-    start: '19:30',
-    end: '20:20'
+    start: '19:30:00.000Z',
+    end: '20:20:00.000Z'
   },
   N3: {
-    start: '20:20',
-    end: '21:10'
+    start: '20:20:00.000Z',
+    end: '21:10:00.000Z'
   },
   N4: {
-    start: '21:20',
-    end: '22:10'
+    start: '21:20:00.000Z',
+    end: '22:10:00.000Z'
   },
   N5: {
-    start: '22:10',
-    end: '23:00'
+    start: '22:10:00.000Z',
+    end: '23:00:00.000Z'
   }
 }
 
@@ -99,13 +99,29 @@ function checkConflicts(reservationStart, reservationEnd, periodStart, periodEnd
     return 2 // The start and / or end is within the period of an existing reservation.
   }
 
+  if (reservation_start.isSame(checkingStart) || reservation_end.isSame(checkingEnd)) {
+    return 3
+  }
+
   return 0
 }
 
 function getConflicts(reservationStart, reservationEnd, callback) {
-  EventCalendar.getAllEvents(function (err, evnt){
-    callback(err, evnt)
+  let conflicts = []
+  EventCalendar.getEventsAtInterval(req.params.startDate, req.params.finalDate, function (err, evnt){
+    if(err){
+      console.log(err)
+      callback('Error retrieving data from database', null)
+    }
+    else{
+      for (var i = 0; i < evnt.length; i++) {
+        if (checkConflicts(reservationStart, reservationEnd, evnt[i].startDate, evnt[i].finalDate)) {
+          conflicts.push(evnt[i])
+        }
+      }
+      callback(null, conflicts)
+    }
   })
 }
 
-module.exports = {classSchedules, checkValid, getConflicts}
+module.exports = {checkValid, getConflicts}
