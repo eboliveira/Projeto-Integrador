72722e4a385a494284b8ccef91693561c8b7ac34
eduh1997 <eduardooliveira.1997@alunos.utfpr.edu.br>
2018-09-20 15:10:48 -0300

    teste in main.js removed, last update correction

--- application/src/main.js
+++ application/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-const teste = require('./services/PostsService')
 
 import VTooltip from 'v-tooltip';
 Vue.use(VTooltip);
@@ -33,15 +32,4 @@ new Vue({
   el: '#app',
   render: h => h(App),
   router
-})
-
-teste.postEvent({
-	"title":"Aniversário",
-	"description": "Meu aniversário de 2018",
-	"room" : "B102",
-	"type_room" : "Laboratorio",
-	"startDate": "2019-12-25T18:00:00Z",
-	"finalDate": "2019-12-25T22:00:00Z",
-	"schedule" :["5m2","5m3"],
-	"responsable" : "Eduardo"
 })
