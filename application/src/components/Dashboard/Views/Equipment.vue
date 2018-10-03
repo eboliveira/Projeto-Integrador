303d0a4193d06cc4af09275a89ac6c57dd47cb44
Elivelton Fernando <eliveltonfoliveira@gmail.com>
2018-10-03 15:17:40 -0300

    Creation of Equipaments file on application

---
+++ application/src/components/Dashboard/Views/Equipment.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <card class="card" style="padding: 15px;">
+                    <h4 slot="header" class="card-title">Reservas</h4>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <v-layout justify-center>
+                                <div class="col-3">
+                                    <b-button class="btn-lg" variant="primary" to="/admin/reserveEquipment">Gerenciar Equipamentos</b-button>
+                                </div>
+                                <div class="col-1"></div>
+                                <div class="col-3">
+                                    <b-button class="btn-lg" variant="primary">Reservar Equipamentos</b-button>
+                                </div>
+                            </v-layout>
+                        </div>
+                    </div>
+                </card>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Card from 'src/components/UIComponents/Cards/Card.vue'
+
+  export default {
+    components: {
+      Card
+    }
+  }
+</script>
