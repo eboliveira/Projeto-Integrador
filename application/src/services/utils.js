d698e85050221941a2064fa25504c148b5402d1a
dsambugaro <daniloc@alunos.utfpr.edu.br>
2018-10-21 20:46:29 -0300

    remove unused function

--- application/src/services/utils.js
+++ application/src/services/utils.js
@@ -1,25 +1,5 @@
 import moment from 'moment'
 
-
-export function RoomBinarySearch(start, end, vector, element) {
-  var guess = Math.floor((start+end) / 2)
-
-  if (end < start) {
-    return -1
-  }
-
-  if (vector[guess].room == element) {
-    return guess
-  }
-
-  if (vector[guess] < element) {
-    return RoomBinarySearch(guess+1, end, vector, element)
-  }
-
-  return RoomBinarySearch(start, guess-1, vector, element)
-
-}
-
 export function checkConflicts(reservationStart, reservationEnd, periodStart, periodEnd) {
   var reservation_start = moment(new Date(reservationStart))
   var reservation_end = moment(new Date(reservationEnd))
