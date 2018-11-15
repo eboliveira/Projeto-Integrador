f1c254767bc97aebffbb71753f49778d25437d29
dsambugaro <daniloc@alunos.utfpr.edu.br>
2018-11-15 15:25:57 -0200

    function to hash string in to hex color and function to transform hex color to hsl color values

--- application/src/services/utils.js
+++ application/src/services/utils.js
@@ -171,3 +171,50 @@ export async function getTime() {
         return moment(new Date(month + '/' + day + '/' + year + ' ' + hours + ':' + minutes))
     })
 }
+
+
+export function hashCode(str) {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+       hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+}
+
+export function intToRGB(i){
+    var c = (i & 0x00FFFFFF)
+        .toString(16)
+        .toUpperCase();
+
+    return "00000".substring(0, 6 - c.length) + c;
+}
+
+export function toHSL(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+    var r = parseInt(result[1], 16);
+    var g = parseInt(result[2], 16);
+    var b = parseInt(result[3], 16);
+
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+        h = s = 0; // achromatic
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    h = Math.round(360*h);
+    s = Math.round(100*s);
+    l = Math.round(100*l);
+    return [h, s, l]
+}
