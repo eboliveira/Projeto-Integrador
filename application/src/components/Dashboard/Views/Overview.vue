821ccbf9c0b4aa14e6587e8899fdebb269a9e91e
dsambugaro <daniloc@alunos.utfpr.edu.br>
2018-12-12 00:20:55 -0200

    fix user importe

--- application/src/components/Dashboard/Views/Overview.vue
+++ application/src/components/Dashboard/Views/Overview.vue
@@ -21,7 +21,7 @@
                         </div>
                         <div slot="content">
                             <p class="card-category">Eventos de hoje</p>
-                            <h4 class="card-title">{{  }}</h4>
+                            <h4 class="card-title">{{ eventsToday }}</h4>
                         </div>
                     </stats-card>
                 </div>
@@ -85,7 +85,8 @@ export default {
     },
     data () {
         return {
-            NumPendents: null
+            NumPendents: null,
+            eventsToday: 0
         }
     },
     mounted: function () {
