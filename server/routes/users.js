e81233ee784cd68dfd7b6af1770de064bf05c33d
dsambugaro <daniloc@alunos.utfpr.edu.br>
2018-12-11 23:23:39 -0200

    change some errors response

--- server/routes/users.js
+++ server/routes/users.js
@@ -11,10 +11,12 @@ router.post('/register', (req,res) => {
             console.log(err.code);
             console.log(err.message);
             if (err.message == 'E-mail domain not authorized') {
-                return res.status(401).send(err.message)
+                return res.status(403).send(err.message)
             } else if (err.code == 'app/invalid-credential') {
-                return res.status(500).send('Internal Server Error')
-            } else if (err.code == 'auth/uid-already-exists' || err.code == 'auth/invalid-email' || err.code == 'auth/email-already-exists' || err.code == 'auth/operation-not-allowed' || err.code == 'auth/weak-password') {
+                return res.status(401).send('Internal Server Error')
+            } else if (err.code == 'auth/operation-not-allowed') {
+                return res.status(405).send(err.code)
+            } else if (err.code == 'auth/uid-already-exists' || err.code == 'auth/invalid-email' || err.code == 'auth/email-already-exists' || err.code == 'auth/weak-password') {
                 return res.status(409).send(err.code)
             } else {
                 return res.status(400).send('server could not understand the request ' + err.message)
